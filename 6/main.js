992403d4c6e4e3b6738fd59a9b62331b7577ba88
KIM-JS-95 <baugh248730@gmail.com>
2022-04-06 09:39:03 +0900

    123

--- 6/main.js
+++ 6/main.js
@@ -1,12 +1,23 @@
-var http = require('http');
-var fs = require('fs');
-var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(url == '/'){
-      url = '/index.html';
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
-
-});
-app.listen(3000);
+var mySwiper = new Swiper('.portfolio .swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',		// 가로 슬라이드
+    loop: true,
+    slidesPerView: 3,		// 한 영역에 보이는 슬라이드 수
+    spaceBetween: 16,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
