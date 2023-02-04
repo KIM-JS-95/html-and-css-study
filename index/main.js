a8283c6bed0050619128c8b59f5e73c83df3c345
KIM-JS-95 <baugh248730@gmail.com>
2023-02-04 19:46:30 +0900

    123

--- index/main.js
+++ index/main.js
@@ -1,16 +1,15 @@
-const { app, BrowserWindow } = require('electron');
-
-function createWindow () {  // 브라우저 창을 생성
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
+const http = require('http');
+var fs = require('fs');
+http.createServer((request, response) => {
+    var url = request.url;
+    if (request.url == '/') {
+      url = '/index.html';
     }
-  })
+    if (request.url == '/favicon.ico') {
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+  }).listen(3000);
 
-  //브라우저창이 읽어 올 파일 위치
-  win.loadFile('./index.html')
-}
-
-app.on('ready', createWindow);
+console.log('Server running at http://127.0.0.1:3000/');
